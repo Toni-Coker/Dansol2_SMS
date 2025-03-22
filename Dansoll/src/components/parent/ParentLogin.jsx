2bf457f9ca7d5dc5eefcb1976ae8f07a2d1d699a
TboyxfireXD <oluwatonilobacoker@gmail.com>
2025-03-22 22:12:29 +0100

    added css, improved styling

--- Dansoll/src/components/parent/ParentLogin.jsx
+++ Dansoll/src/components/parent/ParentLogin.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import './ParentLogin.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./login.css"; // Login page styling
 
 const ParentLogin = () => {
   return (
-    <div>
-        <div className="login-container">
-          <div className="header">
-            <h1 className="heading">Log In</h1>
-          </div>
-          <div className="login-field">
-            <form>
-              <label htmlFor="school-id">School ID</label>
-              <input type="text" id="school-id" placeholder="Input ID here" />
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                placeholder="Input Password here"
-              />
-              <Link to={"/parent"}>
-                <button className="login-button" type="submit">
-                  Log In
-                </button>
-              </Link>
-            </form>
-          </div>
-        </div>
+    <div className="login-container">
+      <div className="header">
+        <h1 className="heading">Log In</h1>
+      </div>
+      <div className="login-field">
+        <form>
+          <label htmlFor="school-id">School ID</label>
+          <input type="text" id="school-id" placeholder="Input ID here" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Input Password here"
+          />
+          <Link to="/parent">
+            <button className="login-button" type="submit">
+              Log In
+            </button>
+          </Link>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default ParentLogin
+export default ParentLogin;
