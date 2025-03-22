f3fe0a84dcad60dddd08cda144397150d439d280
TboyxfireXD <oluwatonilobacoker@gmail.com>
2025-03-22 11:01:08 +0100

    added react bootstrap, redesign on home page, added navbar

--- Dansoll/src/App.jsx
+++ Dansoll/src/App.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './Home'
-import ErrorPage from './ErrorPage'
-import Foot from './Foot'
-import Login from './components/Login'
-import Admin from './components/admin/Admin'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import ErrorPage from "./ErrorPage";
+import Foot from "./Foot";
+import Login from "./components/Login";
+import Admin from "./components/admin/Admin";
+import Navs from "./Navs";
 
 const App = () => {
   return (
-    <div>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/admin' element={<Admin />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='*' element={<ErrorPage />}></Route>
-      </Routes>
-      <Foot />
-    </Router>
+    <div className="app-container">
+      <Router>
+        <Navs /> {/* Navbar at the top */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </div>
+        <Foot /> {/* Footer at the bottom */}
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Dansoll/src/Navs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navs.css"; 
+import dansol from './assets/dansol_logo-removebg-preview.png'
+
+const Navs = () => {
+  return (
+    <nav className="navbar">
+      {/* Logo */}
+      <div className="logo">
+        <img src={dansol} alt="Dansol Logo" />
+      </div>
+
+      {/* Title */}
+      <h1 className="title">Dansol High School App</h1>
+    </nav>
+  );
+};
+
+export default Navs;
